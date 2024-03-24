1b3c2da31c74470c7716341f07a4aae0dde9f84c
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2024-03-24 22:59:46 +0200

    little fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,75 +1,65 @@
 import { NavLink, Outlet } from 'react-router-dom';
-import css from '../components/Page/Page.module.css';
+import css from '../components/Page/StyledPage/HomePage.module.css';
 import { Suspense } from 'react';
-import { useMediaQuery } from 'react-responsive';
 
 export const Home = () => {
-  const isMobile = useMediaQuery({ maxWidth: 450 }) ? true : false;
   return (
     <div className={css.Container}>
       <header>
         <h2>Вітаємо в Cinema Room</h2>
         <p>
-          Тут ви зможете знайти для себе фільм або серіал для перегляду<br></br>
-          в вкладках Фільми та Серіали відповідно, або можете ознайомитись з
-          авторським списком<br></br>
-          рекомендованих фільмів та серіалів
+          Тут ви зможете знайти для себе фільм або серіал для перегляду в
+          вкладках Фільми та Серіали відповідно,
+          <br></br>або можете ознайомитись з авторським списком рекомендованих
+          фільмів та серіалів
         </p>
-        <h2>Авторський список рекомендованих фільмів та серіалів</h2>
       </header>
-      <div className={css.Buttons}>
-        <section>
-          <div>
-            <NavLink
-              to="recommended-movies"
-              style={({ isActive }) => ({
-                background: isActive ? '#be4040' : '#313131',
-              })}
-            >
-              Фільми
-            </NavLink>
-            <NavLink
-              to="recommended-cartoons"
-              style={({ isActive }) => ({
-                background: isActive ? '#be4040' : '#313131',
-              })}
-            >
-              Мульфільми
-            </NavLink>
-            {isMobile && (
-              <>
-                <br></br>
-                <br></br>
-              </>
-            )}
-            <NavLink
-              to="recommended-serials"
-              style={({ isActive }) => ({
-                background: isActive ? '#be4040' : '#313131',
-              })}
-            >
-              Серіали
-            </NavLink>
-            <NavLink
-              to="recommended-cartoon-series"
-              style={({ isActive }) => ({
-                background: isActive ? '#be4040' : '#313131',
-              })}
-            >
-              Мультсеріали
-            </NavLink>
-            <NavLink
-              to="recommended-anime"
-              style={({ isActive }) => ({
-                background: isActive ? '#be4040' : '#313131',
-              })}
-            >
-              Аніме
-            </NavLink>
-          </div>
-        </section>
-      </div>
-
+      <section className={css.Buttons}>
+        <div>
+          <NavLink
+            to="recommended-movies"
+            style={({ isActive }) => ({
+              background: isActive ? '#be4040' : '#313131',
+            })}
+          >
+            Рекомендовані фільми
+          </NavLink>
+          <NavLink
+            to="recommended-cartoons"
+            style={({ isActive }) => ({
+              background: isActive ? '#be4040' : '#313131',
+            })}
+          >
+            Рекомендовані мульфільми
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="recommended-serials"
+            style={({ isActive }) => ({
+              background: isActive ? '#be4040' : '#313131',
+            })}
+          >
+            Рекомендовані серіали
+          </NavLink>
+          <NavLink
+            to="recommended-cartoon-series"
+            style={({ isActive }) => ({
+              background: isActive ? '#be4040' : '#313131',
+            })}
+          >
+            Рекомендовані мультсеріали
+          </NavLink>
+          <NavLink
+            to="recommended-anime"
+            style={({ isActive }) => ({
+              background: isActive ? '#be4040' : '#313131',
+            })}
+          >
+            Рекомендовані аніме
+          </NavLink>
+        </div>
+      </section>
       <Suspense>
         <Outlet />
       </Suspense>
--- src/pages/Serials.jsx
+++ src/pages/Serials.jsx
@@ -1,13 +1,15 @@
 import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
-import css from '../components/Page/Page.module.css';
+import css from '../components/Page/StyledPage/Page.module.css';
 
 export const Serials = () => {
+  const isMobile = useMediaQuery({ maxWidth: 450 }) ? true : false;
   return (
     <div className={css.Container}>
       <div className={css.Buttons}>
-        <section className={css.Category}>
+        <section>
           <h2>Категорії:</h2>
           <div>
             <NavLink
@@ -18,25 +20,51 @@ export const Serials = () => {
             >
               Зараз в тренді
             </NavLink>
-            <NavLink
-              to="popular"
-              style={({ isActive }) => ({
-                background: isActive ? '#be4040' : '#313131',
-              })}
-            >
-              Популярні
-            </NavLink>
-            <NavLink
-              to="top-rated"
-              style={({ isActive }) => ({
-                background: isActive ? '#be4040' : '#313131',
-              })}
-            >
-              Топ рейтинга
-            </NavLink>
+            {!isMobile && (
+              <>
+                <NavLink
+                  to="popular"
+                  style={({ isActive }) => ({
+                    background: isActive ? '#be4040' : '#313131',
+                  })}
+                >
+                  Популярні
+                </NavLink>
+                <NavLink
+                  to="top-rated"
+                  style={({ isActive }) => ({
+                    background: isActive ? '#be4040' : '#313131',
+                  })}
+                >
+                  Топ рейтинга
+                </NavLink>
+              </>
+            )}
           </div>
         </section>
-        <section className={css.Control}>
+        {isMobile && (
+          <section>
+            <div>
+              <NavLink
+                to="popular"
+                style={({ isActive }) => ({
+                  background: isActive ? '#be4040' : '#313131',
+                })}
+              >
+                Популярні
+              </NavLink>
+              <NavLink
+                to="top-rated"
+                style={({ isActive }) => ({
+                  background: isActive ? '#be4040' : '#313131',
+                })}
+              >
+                Топ рейтинга
+              </NavLink>
+            </div>
+          </section>
+        )}
+        <section>
           <h2>Управління:</h2>
           <div>
             <NavLink
